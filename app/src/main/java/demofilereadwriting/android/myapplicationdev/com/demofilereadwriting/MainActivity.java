1bfc90849c6178462bb4a33d578e824ee643a079
15004557 <H0peyur3gd7>
2017-07-20 12:52:45 +0800

    Working example - read and write into external storage

--- app/src/main/java/demofilereadwriting/android/myapplicationdev/com/demofilereadwriting/MainActivity.java
+++ app/src/main/java/demofilereadwriting/android/myapplicationdev/com/demofilereadwriting/MainActivity.java
@@ -1,20 +1,53 @@
 package demofilereadwriting.android.myapplicationdev.com.demofilereadwriting;
 
+import android.Manifest;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    Button btnWrite, btnRead;
+    String folderLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String folderLocation = Environment.getExternalStorageDirectory()
+        btnWrite = (Button) this.findViewById(R.id.btnWrite);
+        btnRead = (Button) this.findViewById(R.id.btnRead);
+        tv = (TextView) this.findViewById(R.id.tv);
+
+
+        int permissionCheck = PermissionChecker.checkSelfPermission
+                (MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            // stops the action from proceeding further as permission not
+            //  granted yet
+            return;
+        }
+
+
+
+        folderLocation = Environment.getExternalStorageDirectory()
                 .getAbsolutePath() + "/Test";
 
         File folder = new File(folderLocation);
@@ -24,6 +57,57 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("File Read/Write", "Folder created");
             }
         }
+
+        //appends into the file
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                File targetFile = new File(folderLocation, "data.txt");
+
+                try {
+                    FileWriter writer = new FileWriter(targetFile, true);
+                    writer.write("Hello world"+"\n");
+                    writer.flush();
+                    writer.close();
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to write!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        //reads into the file
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                File targetFile = new File(folderLocation, "data.txt");
+
+                if (targetFile.exists() == true){
+                    String data ="";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+                        String line = br.readLine();
+                        while (line != null){
+                            data += line + "\n";
+                            line = br.readLine();
+                        }
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!",
+                                Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    tv.setText(data);
+                }
+
+            }
+        });
+
 
     }
 }
